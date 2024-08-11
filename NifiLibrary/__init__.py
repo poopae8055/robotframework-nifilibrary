a6c70b417dc014f535be16785a7b2b0b2414b4ce
poopae8055 <poopae1322@gmail.com>
2024-08-11 20:17:46 +0700

    update requirements and document

--- NifiLibrary/__init__.py
+++ NifiLibrary/__init__.py
@@ -7,19 +7,21 @@ __author__ = 'Weeraporn.pai'
 __email__ = 'poopae1322@gmail.com'
 
 class NifiLibrary(NifiLibrary):
-    """NifiLibrary is a robotframework library for calling jenkins api
+    """NifiLibrary is a robotframework library for calling nipyapi sdk.
 
     == Example Test Cases ==
     | *** Settings ***       |
     | Library                | NifiLibrary   |
+    | Library                | OperatingSystem   |
     |                        |
     | *** Test Cases ***     |
-    | create nifi session | ${protocol}      | ${host}          | ${username}      | ${password}      | ${verify} |
-    | ${job_details}=        | Get Jenkins Job  | ${job_full_name} |
-    | ${job_build_details}=  | Get Jenkins Job Build | ${job_full_name} | ${build_number}  |
-    | ${build_number}=       | Build Jenkins With Parameters | ${job_full_name} | ${parameters_string} |
-    | ${job_build_details}=  | Build Jenkins With Parameters And Wait Until Job Done | ${job_full_name} | ${parameters_string} | 10 | 2 |
-
+    | Connect to Nifi  | ${base_url}  | ${username} | ${password} |
+    | Stop Process Group  | ${rename_processor_group_id} |
+    | Update Parameter Value With Stopped Component  | ${parameter_context_id}  | ${file_filter_param}  | ${file_filter_name} |
+    | Update Parameter Value With Stopped Component  | ${parameter_context_id}  | ${file_name_param}  | ${file_name_value} |
+    | Run Once Processor  | ${rename_file_starter_id} |
+    | ${dic} | List Directory  | ${local_folder_path} |
+    | Wait Until Keyword Succeeds  | 3x  | 5s  | File Should Exist  | ${local_folder_path}/${file_name_value} |
     """
 
     ROBOT_LIBRARY_SCOPE = "GLOBAL"
